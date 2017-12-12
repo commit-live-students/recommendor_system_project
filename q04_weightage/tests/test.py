ea58c348caea0283b7d3c7d7fb81706f9fadec47
abhisheksubu92 <abhisheksubu92@gmail.com>
2017-12-12 15:17:48 +0530

    Update test.py

--- q04_weightage/tests/test.py
+++ q04_weightage/tests/test.py
@@ -11,19 +11,13 @@ class Testing(unittest.TestCase):
     #  Check the arguements of the function
     def test_recommendor_args(self):
         # Input parameters tests
-        args = getargspec(student)
-        self.assertEqual(len(args[0]), 1, "Expected argument(s) %d, Given %d" % (1, len(args)))
-
-    def test_recommendor_defaults(self):
-        args = getargspec(student)
-        self.assertEqual(args[3], (None), "Expected default values do not match given default values")
+        args = getargspec(student).args
+        self.assertEqual(len(args), 1, "Expected argument(s) %d, Given %d" % (1, len(args)))
 
 
     def test_return_dataframe(self):
         self.student_func = student
         self.data = 'data/subreddit-interactions-for-25000-users.zip'
         self.student_return = self.student_func(self.data)
-        self.assertEqual(self.student_return, (140000, 4),
+        self.assertEqual(self.student_return.shape, (700000, 4),
                            "The return values do not match expected values")
-
-   
