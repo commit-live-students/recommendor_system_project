ba1a72a7e5ddd6ce0cbc75f1d0e088f8a654c700
abhisheksubu92 <abhisheksubu92@gmail.com>
2017-12-12 15:16:30 +0530

    Update test.py

--- q05_groupby_users_subreddit/tests/test.py
+++ q05_groupby_users_subreddit/tests/test.py
@@ -1,6 +1,6 @@
 import unittest
 from inspect import getargspec
-from ..build import q05_groupby_users_subreddit as student 
+from ..build import q05_groupby_users_subreddit as student
 from greyatomlib.recommendor_system_project.q05_groupby_users_subreddit.build import q05_groupby_users_subreddit as original
 import dill
 import pandas as pd
@@ -8,7 +8,7 @@ from pandas.util.testing import assert_frame_equal
 
 
 class Testing(unittest.TestCase):
-       
+
     #  Check the arguements of the function
     def test_recommendor_args(self):
         # Input parameters tests
@@ -18,13 +18,11 @@ class Testing(unittest.TestCase):
     def test_recommendor_defaults(self):
         args = getargspec(student)
         self.assertEqual(args[3], (None), "Expected default values do not match given default values")
-    
+
 
     def test_return_dataframe(self):
         self.student_func = student
         self.data = 'data/subreddit-interactions-for-25000-users.zip'
         self.student_return = self.student_func(self.data)
-        self.assertEqual(self.student_return, (140000, 4),
+        self.assertEqual(self.student_return.shape, (216514, 3),
                            "The return values do not match expected values")
-
-
