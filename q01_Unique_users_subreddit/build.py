dcf9e92c8654deb677cfb3a5a520e6458765aa0c
PBPatil <theprasadpatil@gmail.com>
2018-05-13 07:04:48 +0000

    Done

--- q01_Unique_users_subreddit/build.py
+++ q01_Unique_users_subreddit/build.py
@@ -1,7 +1,14 @@
+# %load q01_Unique_users_subreddit/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+path = 'data/subreddit-interactions-for-25000-users.zip'
+def q01_Unique_users_subreddit(path):
+    data = pd.read_csv(path,compression='zip')
+    variable1 = len(data.username.unique())
+    variable2 = len(data.subreddit.unique())
+    return data,variable1,variable2
 
-def q01_Unique_users_subreddit():
+
 
