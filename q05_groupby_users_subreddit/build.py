6d0f81323a0fa220555ac72004931d92161bca5c
tracedence <shoaibrashid887@gmail.com>
2019-01-18 12:53:51 +0000

    Done

--- q05_groupby_users_subreddit/build.py
+++ q05_groupby_users_subreddit/build.py
@@ -1,7 +1,17 @@
+# %load q05_groupby_users_subreddit/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.recommendor_system_project.q04_weightage.build import q04_weightage
 
-def q05_groupby_users_subreddit():
+def q05_groupby_users_subreddit(path):
+    
+    df = q04_weightage(path)
+    df1 = df.groupby(['username','subreddit'])['weights'].sum().reset_index()
+    
+    return df1
+
+path = 'data/subreddit-interactions-for-25000-users.zip'
+q05_groupby_users_subreddit(path)
+
 
