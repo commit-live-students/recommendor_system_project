20a4f0d389d5bafb188bfd0eb088e505f3a67f12
tracedence <shoaibrashid887@gmail.com>
2019-01-18 12:29:49 +0000

    Done

--- q04_weightage/build.py
+++ q04_weightage/build.py
@@ -1,6 +1,24 @@
+# %load q04_weightage/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from greyatomlib.recommendor_system_project.q01_Unique_users_subreddit.build import q01_Unique_users_subreddit
 
-def q04_weightage():
+def q04_weightage(path):
+    
+    df, u_user, u_subreddit = q01_Unique_users_subreddit(path)
+    # minimum value in utc
+    mininum = min(df['utc'])
+    #maximum value in utc for normalization
+    maximum = max(df['utc'])
+    
+    # creating weight column
+    df['weight'] = ((df['utc'] - mininum)+1)/maximum
+    
+    return df
+    
+
+path = 'data/subreddit-interactions-for-25000-users.zip'
+q04_weightage(path)
+
+
