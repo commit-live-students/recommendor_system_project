493c354462aa4f8935747d956f9c3194b5a2da9f
sannidh <soniknitr@gmail.com>
2019-01-31 01:57:54 +0000

    Done

--- q01_Unique_users_subreddit/build.py
+++ q01_Unique_users_subreddit/build.py
@@ -1,7 +1,14 @@
+# %load q01_Unique_users_subreddit/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from greyatomlib.recommendor_system_project.q01_Unique_users_subreddit.build import q01_Unique_users_subreddit as original
+
+path = 'data/subreddit-interactions-for-25000-users.zip'
+
+def q01_Unique_users_subreddit(path):
+    data = pd.read_csv(path, compression = 'zip')
+    return data, np.unique(data.username).shape[0], np.unique(data.subreddit).shape[0]
 
 
-def q01_Unique_users_subreddit():
 
