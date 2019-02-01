73a799e7b4e00c9e8ed43106a70def8e76e164ef
sannidh <soniknitr@gmail.com>
2019-02-01 02:42:45 +0000

    Done

--- q02_top_subreddits_wordcloud/build.py
+++ q02_top_subreddits_wordcloud/build.py
@@ -1,10 +1,20 @@
+# %load q02_top_subreddits_wordcloud/build.py
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from sklearn.model_selection import train_test_split
 from greyatomlib.recommendor_system_project.q01_Unique_users_subreddit.build import q01_Unique_users_subreddit
 
-def q02_top_subreddits_wordcloud():
+path = 'data/subreddit-interactions-for-25000-users.zip'
+
+def q02_top_subreddits_wordcloud(path):
+    data, users, subred = q01_Unique_users_subreddit(path)
+    cloud = WordCloud(background_color='white',width=1600,height=800,relative_scaling=0.5,normalize_plurals=True)
+    wordcloud = cloud.generate_from_frequencies(data['subreddit'].value_counts())
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+
 
 
